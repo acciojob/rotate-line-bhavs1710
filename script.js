da5dfcd2df17a4b00a94382d8c57195400b69fe1
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-05-25 13:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const container = document.getElementsByClassName("container")[0];
+const button = document.getElementById("start-stop");
+function show() {
+    container.innerHTML = '';
+    const div = document.createElement("div")
+    div.id = "line";
+    container.append(div);
+}
+function start() {
+    const line = document.getElementById("line");
+    if (line.className) {
+        line.className = "";
+        button.innerText = "Start";
+    }
+    else {
+        line.className = "line";
+        button.innerText = "stop";
+    }
+}
